325a5a76bf061ce54b996f11e432a11d8571f7e4
harshabonu <harshabonu2003@gmail.com>
2024-05-28 11:52:23 +0530

    changes

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@ const corsOptions = {
   origin: [
     "http://localhost:3000",
     "http://localhost:8080",
+    "https://retail-x-one.vercel.app",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
